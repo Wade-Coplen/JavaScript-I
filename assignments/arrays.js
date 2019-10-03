89faf9c34065a6e18e6f2f5381d54a63ced60ebc
wade coplen <wadecoplen@gmail.com>
2019-10-02 19:45:48 -0700

    start functions

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -74,7 +74,11 @@ let inventory = [
 // [12, 14]
 
 // ==== Challenge 1 ====
-// The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
+// // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year,
+//  make, and model in the console log provided to you below:
+ for () {
+   
+ }
 console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*`);
 
 // ==== Challenge 2 ====
