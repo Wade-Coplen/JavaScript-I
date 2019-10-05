ff6f8848ab224605d3dfb322f59e77cccf823cee
wade coplen <wadecoplen@gmail.com>
2019-10-05 10:06:34 -0700

    After 1:1

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -20,10 +20,10 @@ anotherFunction("Example");
 //   return param1 + param2;
 // };
 // add(1,2);
-let add = (param1, param2) => 
+let add = (param1, param2) => {
     param1 + param2;
 console.log(add(1, 2)); 
-
+}
 //let subtract = function (param1, param2) 
 //   return param1 - param2;
 // };
@@ -40,3 +40,5 @@ console.log(subtract(1,2));
 //   return num * 3;
 // });
 // console.log(triple);
+
+//
